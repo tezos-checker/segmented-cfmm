713ee592cd6a4f931a69b05e397387bac7242e5a
Moremi Vannak <moremi.vannak@serokell.io>
2021-08-03 17:10:11 +0700

    [TCFMM-7] Add auto-generated typescript API
    Problem: We will eventually need to develop a small frontend to be
    able to interact with the contract. To do that, however, we'll first
    need to develop an low-ish level typescript API for that, generated
    from the (Haskell) types.
    
    Solution: Add typescript sdk for SegCFMM, add makefile command, update
    CI.

---
+++ typescript/segmented-cfmm/src/generated/X_to_Y.ts
@@ -0,0 +1,7 @@
+import {Lambda} from '../common';
+export interface X_to_Y {
+  deadline: string;
+  dx: number;
+  min_dy: number;
+  to_dy: string;
+};
---
+++ typescript/segmented-cfmm/src/index.ts
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2021 Arthur Breitman
+// SPDX-License-Identifier: LicenseRef-MIT-Arthur-Breitman
+
+import { TezosToolkit } from '@taquito/taquito';
+import { InMemorySigner, importKey } from '@taquito/signer';
+
+export {Get_time_weighted_sum} from  './generated/Get_time_weighted_sum';
+export {Set_position} from  './generated/Set_position';
+export {X_to_X_prime} from  './generated/X_to_X_prime';
+export {X_to_Y} from  './generated/X_to_Y';
+export {Y_to_X} from  './generated/Y_to_X';
+export { SegCFMMContract } from "./api";
